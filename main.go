76d6276544af632aa7498a933cd60e46064f988f
lum1narie <sn.lum.fallenrose.aol@gmail.com>
2021-05-28 23:05:31 +0900

    関数に切り分けた

--- main.go
+++ main.go
@@ -11,6 +11,10 @@ import (
 	"github.com/jmoiron/sqlx"
 )
 
+var (
+	db *sqlx.DB
+)
+
 type City struct {
 	ID          int    `json:"id,omitempty" db:"ID"`
 	Name        string `json:"name,omitempty" db:"Name"`
@@ -20,18 +24,14 @@ type City struct {
 }
 
 type CountryNamePop struct {
-	Name       string `json:"name,omitempty" db:"Name"`
-	Population int    `json:"population,omitempty" db:"Population"`
+	CountryCode string `json:"countryCode,omitempty" db:"CountryCode"`
+	Name        string `json:"name,omitempty" db:"Name"`
+	Population  int    `json:"population,omitempty" db:"Population"`
 }
 
-func main() {
-	target_city := "Tokyo"
-
-	if len(os.Args) >= 2 {
-		target_city = os.Args[1]
-	}
-
-	db, err := sqlx.Connect("mysql", fmt.Sprintf(
+func initDB() {
+	var err error
+	db, err = sqlx.Connect("mysql", fmt.Sprintf(
 		"%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
 		os.Getenv("DB_USERNAME"), os.Getenv("DB_PASSWORD"),
 		os.Getenv("DB_HOSTNAME"), os.Getenv("DB_PORT"),
@@ -42,20 +42,42 @@ func main() {
 	}
 
 	fmt.Println("Connected!")
+}
+
+func getCity(name string) City {
 	var city City
-	var countryNamePop CountryNamePop
-	if err := db.Get(&city, "select * from city where name = ?", target_city); errors.Is(err, sql.ErrNoRows) {
-		log.Printf("no such city Name %s\n", target_city)
+	if err := db.Get(&city, "select * from city where name = ?", name); errors.Is(err, sql.ErrNoRows) {
+		log.Printf("no such city Name %s\n", name)
 	} else if err != nil {
 		log.Fatalf("DB Error: %s", err)
 	}
+
+	return city
+}
+
+func getCountryNamePop(code string) CountryNamePop {
+	var countryNamePop CountryNamePop
+	if err := db.Get(&countryNamePop, "select country.Name, country.Population where city.Code = ?", code); errors.Is(err, sql.ErrNoRows) {
+		log.Printf("no such country Code %s\n", code)
+	} else if err != nil {
+		log.Fatalf("DB Error: %s", err)
+	}
+
+	return countryNamePop
+}
+
+func main() {
+	initDB()
+
+	target_city := "Tokyo"
+	if len(os.Args) >= 2 {
+		target_city = os.Args[1]
+	}
+
+	city := getCity(target_city)
 	fmt.Printf("%sの人口は%d人です\n", target_city, city.Population)
 
-	if err := db.Get(&countryNamePop, "select country.Name, country.Population from country join city on country.Code = city.CountryCode where city.Name = ?", target_city); errors.Is(err, sql.ErrNoRows) {
-		log.Printf("no such city Name %s\n", target_city)
-	} else if err != nil {
-		log.Fatalf("DB Error: %s", err)
-	}
+	countryNamePop := getCountryNamePop(city.CountryCode)
 
 	ratio := float64(city.Population) / float64(countryNamePop.Population)
 	fmt.Printf("%sの人口は%sの人口の%f%%です\n", target_city, countryNamePop.Name, ratio*100.0)
